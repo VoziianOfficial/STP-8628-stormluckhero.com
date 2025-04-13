6a53e36a6008c9f6aa66ebf608b42a0a6882448a
Yuliia Voziian <vozianulia@gmail.com>
2025-04-14 00:26:15 +0300

    add header section

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,18 @@
 export const header = () => {
+  const openMenuBtn = document.querySelector('.burger_menu-js');
+  const burgerIcon = document.querySelector('.burger-icon');
+  const menu = document.querySelector('.mobile-nav-js');
+  const menuItems = document.querySelectorAll('.mobile-menu-item-js');
 
-}
+  openMenuBtn.addEventListener('click', () => {
+    menu.classList.toggle('is-open');
+    openMenuBtn.classList.toggle('is-open');
+  });
+
+  menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+      menu.classList.remove('is-open');
+      openMenuBtn.classList.remove('is-open');
+    });
+  });
+};
